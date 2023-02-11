09007608a567cb702bfb195ead972a67fe99f728
Eric Wang <ewang7421@gmail.com>
2023-02-11 13:58:08 -0500

    graphs and stats and other player stuff

--- everythinghoops/src/App.js
+++ everythinghoops/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './components/Homepage';
 import Header from "./components/Links";
-import Player from "./components/player";
+import PlayerStats from "./components/player/PlayerStats";
 import './App.css';
+import PlayerGraphs from "./components/player/PlayerGraphs";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}/>
           <Route path="/players" element={<p> I want this to be the url where you search for players</p>}/>
-          <Route path= "/players/:playerId" element={<Player/>}/>
+          <Route path= "/players/:playerId/Stats" element={<PlayerStats/>}/>
+          <Route path= "/players/:playerId/Graphs" element={<PlayerGraphs/>}/>
         </Routes>
       </Router>
     </div>
