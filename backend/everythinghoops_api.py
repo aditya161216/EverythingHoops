89682b7399aad0f01a047c36890520919bf63a9d
justagoodfriend <greenate1@gmail.com>
2023-02-11 20:46:26 -0500

    some random changes

--- backend/everythinghoops_api.py
+++ backend/everythinghoops_api.py
@@ -5,6 +5,7 @@ API to interact with the EverythingHoops database
 import pickle
 import pandas as pd
 
+
 class EverythingHoopsAPI:
     """
     EverythingHoops API
@@ -16,18 +17,22 @@ class EverythingHoopsAPI:
         """
 
         # load pickled dataframes
-        self.players_df = pickle.load(open("backend/data/players_df.pkl", "rb"))
-        self.players_data_df = pickle.load(open("backend/data/players_data_df.pkl", "rb"))
-        self.games_details_df = pickle.load(open("backend/data/games_details_df.pkl", "rb"))
+        self.players_df = pickle.load(
+            open("backend/data/players_df.pkl", "rb"))
+        self.players_data_df = pickle.load(
+            open("backend/data/players_data_df.pkl", "rb"))
+        self.games_details_df = pickle.load(
+            open("backend/data/games_details_df.pkl", "rb"))
         self.games_df = pickle.load(open("backend/data/games_df.pkl", "rb"))
 
     def get_statline(self, pts, ast, reb) -> pd.DataFrame:
         """
         See if a statline exists in the database
         """
-        
+
         #  df of players with statline
-        statline = self.games_details_df[(self.games_details_df["PTS"] >= pts) & (self.games_details_df["AST"] >= ast) & (self.games_details_df["REB"] >= reb)]
+        statline = self.games_details_df[(self.games_details_df["PTS"] >= pts) & (
+            self.games_details_df["AST"] >= ast) & (self.games_details_df["REB"] >= reb)]
 
         # convert nan to 0 if column is float or "" if column is string
         for col in statline.columns:
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -11,19 +11,20 @@ const HomePage = () => {
     const [assists, setAssists] = useState(0);
 
     const nbateams = [];
+    const [data, setData] = useState([{}])
 
-    useEffect( () =>{fetch('http://127.0.0.1:5000/boxscore').then(res=> console.log(res.json())).then (data => console.log(data))}, [])
+    //quick check for itf the data is undefined/didn't match
 
+    //useEffect( () =>
+    //{fetch('http://127.0.0.1:5000/boxscore').then(res=> console.log(res.json())).then (data => setData(data))}, [])
 
+    //if undefined, 
 
-    const beginQuery = async() => {
-
-    }
-
-    //PRA, also options to select in certain date ranges, against a certain team, if these stats have occured, these are optional,
-    //certain team -> placeHolder -> all
-    //certain date -> placeHolder -> range
-    
+    const beginQuery = () => {
+      fetch('http://127.0.0.1:5000/boxscore').then(res => res.json()).then(data => setData(data))
+      console.log(data)
+  
+    }    
 
     return (
           <div className="flex flex-col h-screen w-screen">
@@ -63,7 +64,7 @@ const HomePage = () => {
                   {/* TODO: create the drop down for this */}
               </div>
 
-              <button onClick={beginQuery}>Submit!</button>
+              <button onClick={ () => beginQuery()}>Submit!</button>
           </div>
         </div>
       </div>
