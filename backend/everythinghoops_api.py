422eded9a698b92ec28292a9692457a4bfc4fc22
Sreevatsa03 <sreevatsa.nukala@gmail.com>
2023-02-12 07:13:15 -0500

    added functionality for random best performance

--- backend/everythinghoops_api.py
+++ backend/everythinghoops_api.py
@@ -4,6 +4,7 @@ API to interact with the EverythingHoops database
 
 import pickle
 import pandas as pd
+import random as rnd
 
 class EverythingHoopsAPI:
     """
@@ -97,7 +98,13 @@ class EverythingHoopsAPI:
         best_performance = statline[statline["GAME_SCORE"] == statline["GAME_SCORE"].max()]
 
         # convert best performance to dict
-        best_performance = best_performance.to_dict(orient="records")[0]
+        best_performance = best_performance.to_dict(orient="records")
+
+        # get length of best performance
+        num_best = len(best_performance)
+
+        # select random best performance
+        best_performance = best_performance[rnd.randint(0, num_best - 1)]
 
         # return best performance
         return best_performance
