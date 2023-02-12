af9ebde95dc80a3e91159d2b797628ce77f91201
aditya161216 <59684507+aditya161216@users.noreply.github.com>
2023-02-11 22:29:43 -0500

    updated stats page

--- everythinghoops/src/App.js
+++ everythinghoops/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './components/Homepage';
+import StatsPage from './components/StatsPage';
 import Header from "./components/Links";
 import Datepage from "./components/pages/DateInTime";
 import PlayerStats from "./components/player/PlayerStats";
@@ -14,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}/>
           <Route path="/players" element={<p> I want this to be the url where you search for players</p>}/>
-          <Route path="/performance" element={<Datepage/>}/>
+          <Route path="/performance" element={<StatsPage/>}/>
           <Route path= "/players/:playerId/Stats" element={<PlayerStats/>}/>
           <Route path= "/players/:playerId/Graphs" element={<PlayerGraphs/>}/>
         </Routes>
